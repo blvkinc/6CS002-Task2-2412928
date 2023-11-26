676ad245200476e680f24d0fa1c12e2fdcc6ae61
Rndmun <91401175+Rndmun@users.noreply.github.com>
2023-11-25 20:29:25 -0800

    MultiLingualStringTable.java code refactored

--- base/MultiLingualStringTable.java
+++ base/MultiLingualStringTable.java
@@ -1,25 +1,33 @@
 package base;
 
 /**
+ * Represents a table of multilingual strings.
+ * 
+ * The class provides messages in different languages.
+ * 
  * @author Kevan Buckley, maintained by __student
  * @version 2.0, 2014
  */
-
 public class MultiLingualStringTable {
-	private enum LanguageSetting {
-		English, Klingon
-	}
+    private enum LanguageSetting {
+        English, Klingon
+    }
 
-	private static LanguageSetting cl = LanguageSetting.English;
-	private static String[] em = { "Enter your name:", "Welcome", "Have a good time playing Abominodo" };
-	private static String[] km = { "'el lIj pong:", "nuqneH", "QaQ poH Abominodo" };
+    private static LanguageSetting currentLanguage = LanguageSetting.English;
+    private static String[] englishMessages = { "Enter your name:", "Welcome", "Have a good time playing Abominodo" };
+    private static String[] klingonMessages = { "'el lIj pong:", "nuqneH", "QaQ poH Abominodo" };
 
-	public static String getMessage(int index) {
-		if (cl == LanguageSetting.English) {
-			return em[index];
-		} else {
-			return km[index];
-		}
-
-	}
+    /**
+     * Gets a message from the string table based on the index and current language setting.
+     * 
+     * @param index The index of the message to retrieve
+     * @return The message in the selected language
+     */
+    public static String getMessage(int index) {
+        if (currentLanguage == LanguageSetting.English) {
+            return englishMessages[index];
+        } else {
+            return klingonMessages[index];
+        }
+    }
 }
